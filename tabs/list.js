c97a099a8275d37802f00f80a91609934f5c6098
Shelley Wang <shelleyw13@gmail.com>
2015-08-31 18:50:00 -0700

    Use dummy event data for list view tab

--- tabs/list.js
+++ tabs/list.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var Header = require('../header');
+var Header = require('../header'),
+    React = require('react-native');
 
-var React = require('react-native');
 var {
   StyleSheet,
   Text,
@@ -10,11 +10,60 @@ var {
 } = React;
 
 var listTab = React.createClass({
+  getInitialState: function() {
+    return {data: [],
+            loaded: false};
+  },
+  getDataFromServer: function() {
+    fetch('http://localhost:8000/events.json')
+    .then((response) => response.json())
+    .then((responseData) => {
+      this.setState({
+        events: responseData,
+        loaded: true
+      });
+    })
+    .done();
+  },
+  componentDidMount: function() {
+    this.getDataFromServer();
+  },
+
   render: function() {
-    return (
+     if (!this.state.loaded) {
+       return this.renderLoadingView();
+     }
+     var event = this.state.events[0];
+     return this.renderMovie(event);
+
+   },
+
+   renderLoadingView: function() {
+     return (
       <View style={ styles.container }>
         <Header />
+        <Text>Loading</Text>
       </View>
+     );
+   },
+
+   renderMovie: function(event) {
+     return (
+      <View style={ styles.container }>
+        <Header />
+        <View>
+          <Text >{event.locationId}</Text>
+          <Text >{event.currentActivity}</Text>
+        </View>
+      </View>
+     );
+   }
+});
+
+var ListItem = React.createClass({
+  render: function() {
+    return (
+      <Text>Hello World2</Text>
     );
   }
 });
