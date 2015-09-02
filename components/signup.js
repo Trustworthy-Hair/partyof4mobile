e27cd4eee0eecb7273dfd34db77b30722257fd0e
Shelley Wang <shelleyw13@gmail.com>
2015-09-02 16:37:20 -0700

    Add labels above fields

--- components/signup.js
+++ components/signup.js
@@ -24,28 +24,38 @@ var Signup = React.createClass({
       validUsername: false,
       validEmail: false,
       validPassword: false,
-      validPassword2: true
+      validPassword2: true,
+      showLabels: [false,false,false,false]
     };
   },
   returnToLogin() {
     this.props.onSubmit('login');
   },
+  showLabel: function(num, text) {
+    var showLabels = this.state.showLabels;
+    showLabels[num] = (text.length >num);
+    this.setState({showLabels: showLabels});
+  },
   checkUsername: function(text) {
     var valid = (text.length >= usernameMinLength);
     this.setState({validUsername: valid, username: text});
+    this.showLabel(0, text);
   },
   checkEmail: function(text) {
     var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
     if (re.test(text)) this.setState({validEmail: true, email: text});
+    this.showLabel(1, text);
   },
   checkPassword: function(text) {
     var valid = (text.length >= passwordMinLength);
     if (!/.*(?=.*[a-z])(?=.*[A-Z]).*/.test(text)) valid = false;
     this.setState({validPassword: valid, validPassword2: false, password: text});
+    this.showLabel(2, text);
   },
   checkPassword2: function(text) {
     var valid = (text === this.state.password);
     this.setState({validPassword2: valid, password2: text});
+    this.showLabel(3, text);
   },
   isValid() {
     return (this.state.validUsername && this.state.validEmail && this.state.validPassword && this.state.validPassword2);
@@ -59,7 +69,7 @@ var Signup = React.createClass({
 
     if (this.isValid()) {
       submitButton = (<TouchableHighlight onPress={this.returnToLogin} style={styles.submit}>
-                      <Text style={styles.submitText}>SUBMIT</Text></TouchableHighlight>);
+                      <Text style={styles.submitText}>SIGN UP</Text></TouchableHighlight>);
     } else {
       if (!this.state.validUsername) {
         usernameWarning = this.state.username === '' ? warningGenerator() : warningGenerator(usernameMinLength);
@@ -81,28 +91,39 @@ var Signup = React.createClass({
       }
     }
 
+    var fields = ['username','email','password','retype password'];
+    var labels = [];
+    this.state.showLabels.forEach(function(showLabel, index) {
+      if (showLabel) labels[index] = (<SignupLabel text={fields[index]}/>);
+    });
+
     return (
       <View style={styles.container}>
         <TouchableHighlight onPress={this.returnToLogin} style={styles.back}>
           <Text style={styles.backText}>&lt; Back</Text>
         </TouchableHighlight>
-        <Text>Sign up for PartyOf4!</Text>
+        <Text style={styles.headingText}>Get started with PartyOf4</Text>
+
+        {labels[0]}
         <View style={styles.textInputContainer}>
           <TextInput style={styles.textInput} placeholder='username' maxLength={usernameMaxLength} onChangeText={this.checkUsername}/>
         </View>
         {usernameWarning}
 
+        {labels[1]}
         <View style={styles.textInputContainer}>
           <TextInput style={styles.textInput} placeholder='email' 
           onChangeText={this.checkEmail}/>
         </View>
         {emailWarning}
 
+        {labels[2]}
         <View style={styles.textInputContainer}>
           <TextInput style={styles.textInput} secureTextEntry={true} placeholder='password' onChangeText={this.checkPassword}/>
         </View>
         {passwordWarning}
 
+        {labels[3]}
         <View style={styles.textInputContainer}>
           <TextInput style={styles.textInput} secureTextEntry={true} placeholder='retype password' onChangeText={this.checkPassword2}/>
         </View>
@@ -111,6 +132,16 @@ var Signup = React.createClass({
         {submitButton}
       </View>
     );
+  }
+});
+
+var SignupLabel = React.createClass({
+  render() {
+    return (
+      <View style={styles.label}>
+        <Text style={styles.labelText}>{this.props.text}</Text>
+      </View>
+    ); 
   }
 });
 
@@ -123,17 +154,21 @@ var styles = StyleSheet.create({
     alignItems: 'center'
   },
   back: {
+    marginTop: 20,
     height: 40,
-    width: 300,
-    marginBottom: 10,
+    width: 310,
   },
   backText: {
     fontSize: 20
   },
+  headingText: {
+    fontSize: 17,
+    color: '#2e6a8b'
+  },
   textInputContainer: {
-    width: 250,
+    width: 230,
     height: 40,
-    marginBottom: 10,
+    marginBottom: 5,
     borderWidth: 1,
     borderTopColor: 'white', 
     borderRightColor: 'white',
@@ -143,7 +178,7 @@ var styles = StyleSheet.create({
   },
   textInput: {
     height: 30, 
-    width: 240,
+    width: 220,
     borderWidth: 0,
   },
   warning: {
@@ -151,17 +186,22 @@ var styles = StyleSheet.create({
   },
   submit: {
     backgroundColor: '#2e6a8b',
-    margin: 20,
+    margin: 10,
     height: 40,
-    width: 200,
-    borderRadius: 10,
-    marginBottom: 10,
+    width: 240,
     alignItems: 'center',
     justifyContent: 'center'
   },
   submitText: {
     color: 'white',
     fontSize: 20
+  },
+  label: {
+    width: 250
+  },
+  labelText: {
+    color: '#14203c',
+    fontSize: 12
   }
 });
 
